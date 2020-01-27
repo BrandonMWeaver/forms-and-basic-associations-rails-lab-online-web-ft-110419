676d06cf95ec418db89b4cf39efe45bf48a45f4a
Brandon Weaver <a7xncob@gmail.com>
2020-01-27 23:19:52 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,11 @@ class Song < ActiveRecord::Base
   has_many :notes
   
   def artist_name=(name)
-    
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+  
+  def artist_name
+    return self.artist ? self.artist.name : nil
   end
   
 end
